1ede8b8c6a1453874a51c180ccdda1dd6d4724e3
rafamaciel21 <rafamaciel21@gmail.com>
2023-06-13 14:41:02 -0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            var carro = VeiculoBuilder.builder()
-                    .marca("Chevrolet")
-                    .modelo("Astra")
-                    .ano(2008)
-                    .km(80000)
-                    .valor(23200.02)
-                    .build();
+        var carro = VeiculoBuilder.builder()
+                .marca("Chevrolet")
+                .modelo("Astra")
+                .ano(2008)
+                .km(80000)
+                .valor(23200.02)
+                .build();
 
-            System.out.println(carro);
+        System.out.println(carro);
+
+        var carro2 = VeiculoBuilder.builder()
+                .marca("Chevrolet")
+                .modelo("Cruze")
+                .ano(2008)
+                .build();
+
+        System.out.println(carro2);
 
     }
 }
